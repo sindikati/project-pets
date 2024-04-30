3d52cc018145acc0bef4da71d2f7ffd288c65ca4
t5o7n3 <cotne14@gmail.com>
2024-04-30 13:46:18 +0400

    required inputs

--- add.js
+++ add.js
@@ -5,21 +5,29 @@ const titleInput = document.getElementById('title');
 const imgInput = document.getElementById('image');
 const descriptionInput = document.getElementById('description');
 const contactPersonNameInput = document.getElementById('contactPersonName');
-const contactPersonNumberInput= document.getElementById('contactPersonNumber');
+const contactPersonNumberInput = document.getElementById('contactPersonNumber');
 
 
 
 
 
 const statements = JSON.parse(localStorage.getItem('statements'));
+// const statements = [];
 
 
-addBtn.addEventListener('click', () =>{
-    
+
+addBtn.addEventListener('click', () => {
+    if (titleInput.value === '', imgInput.value === '', descriptionInput.value === '', contactPersonNameInput.value === '', contactPersonNumberInput.value === '') {
+        alert('Please, fill all infromation');
+        return
+    };
+
     const newStatement = new Statement(titleInput.value, contactPersonNameInput.value, contactPersonNumberInput.value, null, descriptionInput.value, null, null, imgInput.value)
     statements.push(newStatement);
-    console.log(newStatement);
     localStorage.setItem('statements', JSON.stringify(statements));
+    window.location.href = 'index.html';
+
+
 });
 
 
