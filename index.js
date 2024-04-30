75662f53aa25b0295c97bf458aea94e8108494e7
t5o7n3 <cotne14@gmail.com>
2024-04-30 12:04:39 +0400

    added search input

--- index.js
+++ index.js
@@ -1,12 +1,19 @@
 
 
 const petList = document.querySelector('.pet-list');
+const searchInput = document.querySelector('.search-input');
+const searchBtn = document.querySelector('.search-btn');
 
 
 const statement1 = new Statement('ჩუქდება თეთრი კნუტი 1 თვის','ლუკა','555555555','თბილისი','არის საყვარელი',null,0,'https://www.catster.com/wp-content/uploads/2023/12/White-British-Shorthair_OksanaSusoeva-Shutterstock-1024x683-1.jpg');
-const statement2 = new Statement('ჩუქდება თეთრი კნუტი 1 თვის','ცოტნე','555555555','თბილისი','არის საყვარელი',null,0,'https://www.catster.com/wp-content/uploads/2023/12/White-British-Shorthair_OksanaSusoeva-Shutterstock-1024x683-1.jpg');
+const statement2 = new Statement('იყიდება თეთრი კნუტი 1 თვის','ცოტნე','555555555','თბილისი','არის საყვარელი',null,0,'https://www.catster.com/wp-content/uploads/2023/12/White-British-Shorthair_OksanaSusoeva-Shutterstock-1024x683-1.jpg');
+const statement3 = new Statement('ჩუქდება შავი კნუტი 1 თვის','ცოტნე','555555555','თბილისი','არის საყვარელი',null,0,'https://www.catster.com/wp-content/uploads/2023/12/White-British-Shorthair_OksanaSusoeva-Shutterstock-1024x683-1.jpg');
+const statement4 = new Statement('იყიდება შავი კნუტი 1 თვის','ცოტნე','555555555','თბილისი','არის საყვარელი',null,0,'https://www.catster.com/wp-content/uploads/2023/12/White-British-Shorthair_OksanaSusoeva-Shutterstock-1024x683-1.jpg');
+const statement5 = new Statement('ჩუქდება შავი კნუტი 1 თვის','ცოტნე','555555555','თბილისი','არის საყვარელი',null,0,'https://www.catster.com/wp-content/uploads/2023/12/White-British-Shorthair_OksanaSusoeva-Shutterstock-1024x683-1.jpg');
+const statement6 = new Statement('იყიდება შავი კნუტი 1 თვის','ცოტნე','555555555','თბილისი','არის საყვარელი',null,0,'https://www.catster.com/wp-content/uploads/2023/12/White-British-Shorthair_OksanaSusoeva-Shutterstock-1024x683-1.jpg');
 
-const statementArray = [statement1,statement2];
+
+const statementArray = [statement1,statement2,statement3,statement4,statement5,statement6];
 
 const appendStatement = (statement) =>{
     const statementTemplate = `
@@ -26,5 +33,27 @@ const appendAllStatements = (statements) =>{
         appendStatement(statement);
     }
 };
+
+
+
+searchBtn.addEventListener('click', ()=>{
+    petList.innerHTML = '';
+
+    if(searchInput.value === ""){
+        appendAllStatements(statementArray);
+        return;
+    }
+
+    const filteredArray = statementArray.filter((statement)=>{
+        if (statement.title.includes(searchInput.value)) {
+            return true;
+        } else {
+            return false;
+        }
+    });
+
+    appendAllStatements(filteredArray);
+});
+
 
 appendAllStatements(statementArray);
